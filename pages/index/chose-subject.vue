d10dddcf290c03a7818ec424048ef309f059b0fa
haoxianhuan <hao3612562@gmail.com>
2020-11-25 22:58:19 +0800

    优化搜索功能

--- pages/index/chose-subject.vue
+++ pages/index/chose-subject.vue
@@ -1,165 +1,180 @@
 <template>
-	<view>
-        <uni-search-bar @confirm="searchLastCategory" placeholder="输入您的科目或者专业代码"></uni-search-bar>
+    <view>
+        <u-search class="search-bar" placeholder="输入您的科目或者专业代码" :show-action="true" v-model="searchValue" @change="searchChange"
+            @custom="searchLastCategory" @search="searchLastCategory" shape="square" height="80" :animation="true"
+            :clearabled="true" @clear="clear"></u-search>
 
         <u-grid :col="3" :border="false" class="customGrid" v-if="!showUniListFormat && categoryList" @click="gridClicked">
-            <u-grid-item  class="customGridItem" v-for="item in categoryList" :index="item.id">
+            <u-grid-item class="customGridItem" v-for="item in categoryList" :index="item.id">
                 <text class="text">{{ item.name }}</text>
             </u-grid-item>
         </u-grid>
 
-        <uni-list v-if="showUniListFormat && categoryList">
-            <uni-list-item v-for="item in categoryList" :showArrow="true" :title="item.name" @click="listItemClicked(item.id)"></uni-list-item>
-        </uni-list>
-	</view>
+        <u-cell-group v-if="showUniListFormat && categoryList">
+            <u-cell-item v-for="item in categoryList" :title="item.name" @click="listItemClicked(item.id)"></u-cell-item>
+        </u-cell-group>
+    </view>
 </template>
 
 <script>
-import uniSearchBar from '@/components/uni-search-bar/uni-search-bar.vue'
-
-import uniGrid from "@/components/uni-grid/uni-grid.vue"
-import uniGridItem from "@/components/uni-grid-item/uni-grid-item.vue"
-
-import uList from "@/components/uni-list/uni-list.vue"
-import uListItem from "@/components/uni-list-item/uni-list-item.vue"
-let cache = []
-export default {
-    components: {
-        uniSearchBar,
-        uniGrid, uniGridItem,
-        uList, uListItem
-    },
-    data() {
-        return {
-            showUniListFormat: false,
-            categoryList: [],
-            col:3
-        }
-    },
-    async onShow () {
-        const self = this
-        const {index} = self.options
-
-        if(index){
-            index >1 ?self.showUniListFormat = true :self.showUniListFormat = false
-            if(cache[index-1]){
-                self.categoryList = cache[index-1]
-            }else{
-               await self.loadCategory(0)
-            }
-        }else if(self.route === 'pages/index/chose-subject') {
-            uni.redirectTo({
-                url: '/pages/index/chose-subject?index=1'
-            })
-        }
-
-    },
-    onUnload(){
-       const self = this
-       const {index} = self.options
-       if(index === '1'){
-           cache = []
-       }
-    },
-    methods: {
-        click(){},
-        itemClick(){},
-        async searchLastCategory (e) {
-            const self = this
-
-            const value = e.value
-
-            if (value == '') {
-                return self.loadCategory(0)
-            }
-
-            const searchCategoryRes = await self.$apiRequest({
-                url: self.$apiList.categorySearchLast,
-                data: {
-                    n: value
-                },
-                header: {
-                    Authorization: 'Bearer ' + self.$store.state.member.memberToken
-                }
-            })
-
-            if (searchCategoryRes.code == 0) {
-                self.categoryList = searchCategoryRes.data
+    let cache = []
+    export default {
+        components: {},
+        data() {
+            return {
+                showUniListFormat: false,
+                categoryList: [],
+                searchValue: '',
+                currentIndex:''
             }
         },
-        async gridClicked (e) {
+        async onShow() {
             const self = this
+            const { index } = self.options
 
-            const categoryId = e
-
-            await self.loadCategory(categoryId)
-        },
-        async listItemClicked (pid) {
-            const self = this
-
-            await self.loadCategory(pid)
-        },
-        async loadCategory (pid) {
-            const self = this
-
-            const res = await self.$apiRequest({
-                url: self.$apiList.categoryIndex,
-                method: 'GET',
-                data: {
-                    pid: pid
-                },
-                header: {
-                    Authorization: 'Bearer ' + self.$store.state.member.memberToken
-                }
-            })
-            if (res.code == 0) {
-                if (res.data.length == 0) {
-                    await self.saveChosenCategory(pid)
+            if (index) {
+                index > 1 ? self.showUniListFormat = true : self.showUniListFormat = false
+                this.currentIndex = index - 1
+                if (cache[this.currentIndex]) {
+                    self.categoryList = cache[this.currentIndex]
                 } else {
-                    let index = cache.length
-                    cache[index] = res.data
-                    index = cache.length
-                    if(index > 1){
-                        uni.navigateTo({
-                            url: '/pages/index/chose-subject?index='+ index
-                        })
-                    }
-                    self.categoryList = res.data
+                    await self.loadCategory(0)
                 }
+            } else if (self.route === 'pages/index/chose-subject') {
+                uni.redirectTo({
+                    url: '/pages/index/chose-subject?index=1'
+                })
             }
 
         },
-        async saveChosenCategory (categoryId) {
+        onUnload() {
             const self = this
-
-            const res = await self.$apiRequest({
-                url: self.$apiList.practiseRecord,
-                method: 'POST',
-                data: {
-                    cid: categoryId
-                },
-                header: {
-                    Authorization: 'Bearer ' + self.$store.state.member.memberToken
+            const {
+                index
+            } = self.options
+            if (index === '1') {
+                cache = []
+            }
+        },
+        methods: {
+            clear() {
+                this.categoryList = cache[this.currentIndex]
+                this.searchValue = ''
+                if(this.currentIndex === 0)this.showUniListFormat = false
+            },
+            searchChange(e) {
+                this.searchValue = e
+                if (!this.searchValue) {
+                    this.categoryList = cache[this.currentIndex]
+                    if(this.currentIndex === 0)this.showUniListFormat = false
                 }
-            })
-
-            if (res.code == 0) {
-                uni.switchTab({
-                    url: '/pages/index/index'
+            },
+            async searchLastCategory(e) {
+                const self = this
+                const value = e
+                self.showUniListFormat = true
+                if (!value) {
+                    this.categoryList = cache[this.currentIndex]
+                    if(this.currentIndex === 0)this.showUniListFormat = false
+                    return
+                }
+                const searchCategoryRes = await self.$apiRequest({
+                    url: self.$apiList.categorySearchLast,
+                    data: {
+                        n: value
+                    },
+                    header: {
+                        Authorization: 'Bearer ' + self.$store.state.member.memberToken
+                    }
                 })
+
+                if (searchCategoryRes.code == 0) {
+                    self.categoryList = searchCategoryRes.data
+                }
+            },
+            async gridClicked(e) {
+                const self = this
+
+                const categoryId = e
+
+                await self.loadCategory(categoryId)
+            },
+            async listItemClicked(pid) {
+                const self = this
+
+                await self.loadCategory(pid)
+            },
+            async loadCategory(pid) {
+                const self = this
+
+                const res = await self.$apiRequest({
+                    url: self.$apiList.categoryIndex,
+                    method: 'GET',
+                    data: {
+                        pid: pid
+                    },
+                    header: {
+                        Authorization: 'Bearer ' + self.$store.state.member.memberToken
+                    }
+                })
+                if (res.code == 0) {
+                    if (res.data.length == 0) {
+                        await self.saveChosenCategory(pid)
+                    } else {
+                        let index = cache.length
+                        cache[index] = res.data
+                        index = cache.length
+                        if (index > 1) {
+                            uni.navigateTo({
+                                url: '/pages/index/chose-subject?index=' + index
+                            })
+                        }
+                        self.categoryList = res.data
+                    }
+                }
+
+            },
+            async saveChosenCategory(categoryId) {
+                const self = this
+
+                const res = await self.$apiRequest({
+                    url: self.$apiList.practiseRecord,
+                    method: 'POST',
+                    data: {
+                        cid: categoryId
+                    },
+                    header: {
+                        Authorization: 'Bearer ' + self.$store.state.member.memberToken
+                    }
+                })
+
+                if (res.code == 0) {
+                    uni.switchTab({
+                        url: '/pages/index/index'
+                    })
+                }
             }
         }
     }
-}
 </script>
 
 <style scoped>
-.customGrid {
-    background-color: #FFFFFF;
-}
+    .customGrid {
+        background-color: #FFFFFF;
+    }
 
-.text {
-    text-align: center;
-    padding: 30px 0;
-}
+    .text {
+        text-align: center;
+        padding: 30px 0;
+    }
+
+    .search-bar {
+        /* #ifndef APP-NVUE */
+        display: flex;
+        /* #endif */
+        flex-direction: row;
+        position: relative;
+        padding: 8px;
+        background-color: #ffffff;
+    }
 </style>
